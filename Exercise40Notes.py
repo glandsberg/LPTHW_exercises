7ac868ba4c9f820855205ca4699e17f8275a2426
grace <58996074+glandsberg@users.noreply.github.com>
2020-11-05 23:22:31 -0500

    Add files via upload
    extra ones I forgot to upload

--- Exercise40Notes.py
+++ Exercise40Notes.py
@@ -3,3 +3,19 @@
 Python is an object oriented programming language 
 Object Oriented Programming = OOP
 learning here about classes and objects through what we know about dictionaries and modules
+
+dictionary = a way to map one thing to another 
+example:
+
+"""
+
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+
+# You can think about a module as a specialized dictionary that can store Python code so you can access
+	# it with the . operator.
+	
+
+
+
+
